afa2640cc90d06f2aa2ecf9bfafd856dcbbbd85a
Rakibuldeveloper <morfiverr@gmail.com>
2024-06-29 18:08:04 +0600

    window-objects

---
+++ window-object.js
@@ -0,0 +1,31 @@
+
+// before using this window-object you need to an index.html file 
+// and connect index.js file, write a tag like <button onclick="write the function name"></button>
+
+
+// this function show you an alert on the window
+
+function objAlert(params) {
+    alert()
+}
+
+// this funciton show you a window where you show two button ok or cancle
+function objConfrim(params) {
+    confirm()
+    
+}
+
+// this function gives you a window, where you put an input
+function objPrompt(params) {
+    prompt()
+}
+
+// this function use to open new tab
+function objOpen(params) {
+    open()
+}
+
+// this function use to close the current tab
+function objClose(params) {
+    close()
+}
